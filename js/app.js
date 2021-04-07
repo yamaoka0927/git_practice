0d9d5bf0ba6a60cb5efdb341ac2412d9d42204ef
境野巧己 <sakaino@gizumo-inc.jp>
2021-04-07 16:39:27 +0900

    二日目作業終了

--- js/app.js
+++ js/app.js
@@ -2,4 +2,10 @@ $(function() {
   $('.layout_head_menu').on('click', function() {
     $(this).children('ul').toggle();
   });
+
+  $('.layout_head_menu').children('ul').children().on('click', function() {
+    var section = '[data-section="' + $(this).data('menu-section') + '"]';
+    var position = Number($(section).offset().top) - 95;
+    $(window).scrollTop(position);
+  });
 });
